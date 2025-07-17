4b56f6b35d4a6bb283f8d744b3dedb3efa111347
berlene <berlenebernabe12@gmail.com>
2025-07-17 18:59:41 +0800

    ADDED ATTENDANCEQR TO APP

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import Events from "./components/Events"; 
+import Events from "./components/Events";
+import AttendanceQR from "./components/AttendanceQR";
 import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
@@ -20,7 +21,8 @@ export default function App() {
       >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
-        <Stack.Screen name="Events" component={Events} /> 
+        <Stack.Screen name="Events" component={Events} />
+        <Stack.Screen name="AttendanceQR" component={AttendanceQR} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
